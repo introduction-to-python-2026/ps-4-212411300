ec8156a4b642d14d9a68bd39bf48a9b57a573c20
212411300 <racheliseitz1@gmail.com>
2025-11-15 15:45:32 +0200

    שיעורי בית 4 גרסא 4

--- string_utils.py
+++ string_utils.py
@@ -3,25 +3,30 @@ def split_before_each_uppercases(formula):
      end = 1
      split_formula = []
 
-     if len(formula) == 0:
+     if not formula:
        return formula
          
      while end < len(formula):
-      if formula [end].isupper():
-        split_formula.append(formula[start:end])
-        start=end
+      if formula[end].isupper():
+         split_formula.append(formula[start:end])
+         start=end
       end +=1
       split_formula.append(formula[start:])
       
      return split_formula
 
 
+
 def split_at_first_digit(formula):
-    def split_at_first_digit(formula):
      digit_location = 1
-     for char in formula: 
+     for char in formula[1:]:
           if char.isdigit ():
               break
           digit_location += 1
-          if digit_location == len (formula):
+     if digit_location == len (formula):
               return formula, 1
+
+
+
+
+
