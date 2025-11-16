534a899684b8e2b1105fb823221c00b64ce284ad
212411300 <racheliseitz1@gmail.com>
2025-11-16 22:15:41 +0200

    שיעורי בית 4 גרסא 8

--- string_utils.py
+++ string_utils.py
@@ -10,7 +10,7 @@ def split_before_each_uppercases(formula):
        if formula[end].isupper():
          split_formula.append(formula[start:end])
          start=end
-         end +=1
+       end +=1
      split_formula.append(formula[start:])
       
      return split_formula
